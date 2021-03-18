09f76ff9ffc18780402cc5d9debc6f8895e8653b
nao-gabu0445 <78404803+nao-gabu0445@users.noreply.github.com>
2021-03-19 01:40:28 +0900

    ユーザー登録ログインログアウト編集機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'user/edit'
+  devise_for :users
+  get 'question/index'
+  get 'comments/index'
+  root to: "question#index"
+  resources :users ,only:[:edit, :update]
 end
